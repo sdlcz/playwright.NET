667da85cb8d423a2e4dc1e1e2d309ea219c2621f
sdlcz <stantonsdlc@gmail.com>
2025-03-02 20:13:12 +0000

    Added MockSwagTest

---
+++ MockSwagTest.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Playwright.NUnit;
+
+namespace NunitPlaywrightTests
+{
+    [Parallelizable(ParallelScope.Self)]
+    [TestFixture]
+    public class MockSwagTest : PageTest
+    {
+        [SetUp]
+        public async Task SetUp()
+        {
+            // Code to set up before each test
+            await Page.GotoAsync("https://www.saucedemo.com/v1/");
+        }
+
+        [Test]
+        public async Task VerifyPageElements ()
+        {
+            var pageTitle = await Page.TitleAsync();
+            Assert.AreEqual("Swag Labs", pageTitle, "Page title should be 'Swag Labs'.");
+        }
+
+        [TearDown]
+        public async Task TearDown()
+        {
+            // Code to run after each test
+            await Page.CloseAsync();
+        }
+    }
+}
